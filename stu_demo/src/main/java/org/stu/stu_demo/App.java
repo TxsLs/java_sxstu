b3ecd3dfededba91d5090762cddef4a968d87a39
TxsLS <l1597757959@163.com>
2023-08-01 15:47:03 +0800

    实训练习

---
+++ stu_demo/src/main/java/org/stu/stu_demo/App.java
@@ -0,0 +1,13 @@
+package org.stu.stu_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
