057f6561716a5ce334e251fb8a0ac6f91a65e6b6
TxsLS <l1597757959@163.com>
2023-08-02 11:37:36 +0800

    练习systemUtils和StringUtils

--- maven.demo/src/main/java/org/stu/maven/demo/App.java
+++ maven.demo/src/main/java/org/stu/maven/demo/App.java
@@ -1,6 +1,9 @@
 package org.stu.maven.demo;
 
+import java.io.File;
+
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 /**
  * Hello world!
@@ -42,9 +45,41 @@ public class App {
 		//		System.out.println("9." + strr + ".");
 		//		System.out.println();
 
-		strr = StringUtils.joinWith("./*-", "123", "456", "789");
-		System.out.println(strr);
+		//	Object[] arr=new Object[] {"123", "456", "7890"};		
+		//		Object[] arr = new Object[4];
+		//		arr[0] = "123";
+		//		arr[1] = "456";
+		//		arr[2] = "789";
+		//		arr[3]="ls";
+		//		strr = StringUtils.joinWith("@", arr);
+		//		System.out.println(strr);
+		//		strr = StringUtils.joinWith("./*-", "123", "456", "789");
+		//		System.out.println(strr);
 
+		strr = SystemUtils.getHostName();
+		System.out.println("主机名：" + strr);
+		strr = SystemUtils.getUserName();
+		System.out.println("用户名：" + strr);
+
+		File file = SystemUtils.getJavaHome();
+		System.out.println("java变量：" + file);
+		file = SystemUtils.getJavaIoTmpDir();
+		System.out.println("java临时目录：" + file);
+		file = SystemUtils.getUserDir();
+		System.out.println("java用户目录：" + file);
+		System.out.println("is java 8:"+SystemUtils.IS_JAVA_1_8);
+		System.out.println("is java 11:"+SystemUtils.IS_JAVA_11);
+		if(SystemUtils.IS_OS_WINDOWS_2003) {
+			System.out.println("win10");
+		}else {
+			System.out.println("bushi");
+		}
+		
+		if(SystemUtils.isJavaAwtHeadless()) {
+			System.out.println("win10");
+		}else {
+			System.out.println("bushi");
+		}
 	}
 
 }
